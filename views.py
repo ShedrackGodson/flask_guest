1c9c3603926223e2717a7c0aa41b20abdd2a8efd
ShedrackGodson <hedrickgodson23@gmail.com>
2020-12-23 16:38:17 +0300

    added static files and base templates where i can extends to the similar ones

--- views.py
+++ views.py
@@ -4,6 +4,15 @@ main = Blueprint("main", __name__) # Instantiating a blue print
 
 @main.route("/") # Routing
 def index():
+    context = dict()
+    context["num1"] = 23
+    context["num2"] = 1997
 
-    return "<h1>Hello Flask. This is me Shedrack Godson</h1>" # Rendering what to be seen on the template
+    return render_template("index.html") # Rendering what to be seen on the template
+
+
+@main.route("/sign")
+def sign():
+   
+    return render_template("sign.html")
 
