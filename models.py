ae42cab801f6b6689d47c891a1b93b7d156bd2f1
ShedrackGodson <hedrickgodson23@gmail.com>
2020-12-23 19:33:13 +0300

    CRUD functionality and messages flushing functionality

--- models.py
+++ models.py
@@ -5,4 +5,10 @@ class Comment(db.Model):
     name = db.Column(db.String(30))
     comment = db.Column(db.String(1000))
 
-    
+    def __repr__(self):
+        return f"{self.name}-{self.comment[:10]}...."
+
+    class Meta:
+        ordering = [
+            "-id"
+        ]
--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import (Blueprint, render_template, request,
+    redirect, url_for, flash
+    )
 from . import db
 from .models import Comment
 
@@ -10,13 +12,13 @@ def index():
     return render_template("index.html", comments_qs=comments_qs) # Rendering what to be seen on the template
 
 
-@main.route("/sign")
+@main.route("/sign/")
 def sign():
    
     return render_template("sign.html")
 
 
-@main.route("/sign", methods=["POST"])
+@main.route("/sign/", methods=["POST"])
 def sign_up():
     name = request.form.get("name")
     comment = request.form.get("comment")
@@ -31,5 +33,41 @@ def sign_up():
     db.session.add(comment_obj)
     db.session.commit()
     # Redirecting to the home page
+    return redirect(url_for("main.index"))
+
+
+@main.route("/sign_delete/<int:id>/", methods=["GET"])
+def sign_delete(id=id):
+    sign_obj = Comment.query.get(id)
+    
+    return render_template("sign_delete.html", sign_obj=sign_obj)
+
+
+@main.route("/sign_delete_post/<int:id>/", methods=["POST"])
+def sign_delete_post(id=id):
+    sign_obj = Comment.query.get(id) # Getting Object
+    db.session.delete(sign_obj) # Delete Object
+    db.session.commit()
+    flash("Deleted successfully.", "success")
+    return redirect(url_for("main.index"))
+
+
+@main.route("/sign_edit/<int:id>/", methods=["GET"])
+def sign_edit(id=id):
+    sign_obj = Comment.query.get(id)
+
+    return render_template("sign_edit.html", sign_obj=sign_obj)
+
+
+@main.route("/sign_edit_request/<int:id>/", methods=["POST"])
+def sign_edit_request(id=id):
+    sign_obj = Comment.query.get(id) # Getting Object
+    name = request.form.get("name")
+    comment = request.form.get("comment")
+    sign_obj.name = name
+    sign_obj.comment = comment
+    db.session.add(sign_obj)
+    db.session.commit()
+    flash("Updated successfully.", "success")
     return redirect(url_for("main.index"))
 
